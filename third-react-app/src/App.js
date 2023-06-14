d6bbd72061416a7313a2bca5273da54d536e5f88
Raviraj patil <ravirajpatil298@gmail.com>
2023-06-14 20:23:20 +0530

    upper lowercase

--- third-react-app/src/App.js
+++ third-react-app/src/App.js
@@ -1,38 +1,33 @@
 import {useState} from "react";
-
 function App(){
   return(
     <>
-    <h1>Counter App</h1>
-  <CounterApp/>
-  <CounterApp/>
-  <CounterApp/>
-  <CounterApp/>
-
-
+    <h1>string Example</h1>
+    <StringDemo/>
     </>
-  )
-};
+  );
+}
 
-function CounterApp(){
-  // let counter=1; // stateless
-  //  let list=useState(1); //stateful
-  //  let [counter,setCounter]=list;
-   let [counter,setCounter] =useState(1);
+function StringDemo(){
 
-   //member function
-   let likeMeAction = () =>{
-      counter = counter+1;
-      console.log(counter);
-      setCounter(counter);
-   }
+  let [title,setTitle] = useState("Hello World");
+
+  let lowerCaseAction = () => {
+    title=title.toLowerCase();
+    console.log(title);
+    setTitle(title);
+  };
+  let upperCaseAction = () => {
+    title=title.toUpperCase();
+    console.log(title);
+    setTitle(title);
+  };
   return(
     <>
-    <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, corrupti eligendi. Aspernatur illo ab veniam deserunt molestiae excepturi repellat qui, animi accusantium quam exercitationem et consectetur sit placeat error rerum.</h5>
-    <h1>{counter} <input type="button" value="&#128077;" onClick={likeMeAction} /></h1>
-  
-    
+    <h1>{title}</h1>
+    <input type="button" value="Lower Case" onClick={lowerCaseAction} />
+    <input type="button" value="Upper Case" onClick={upperCaseAction} />
     </>
-  )
+  );
 }
 export default App;
