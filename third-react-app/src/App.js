470d5c191b883b34138634a77bb3707726e5be88
Raviraj patil <ravirajpatil298@gmail.com>
2023-06-14 12:23:12 +0530

    Counter appli

--- third-react-app/src/App.js
+++ third-react-app/src/App.js
@@ -1,17 +1,32 @@
-import AppBody from "./pages/AppBody";
-import AppFooter from "./pages/AppFooter";
-import AppHeader from "./pages/AppHeader";
-
+import { useState } from "react";
 function App(){
-   return <>
-    <AppHeader/>
-    <AppBody />
-    <AppFooter/>
-   
-   </>
+  return( <>
+   <h1>Counter App</h1>
+   <CounterApp/>
+  </>
+  );
+}
+
+function CounterApp(){
+  let[counter,setCounter] = useState(1);
+  let likeMeAction=()=>{
+    counter =counter + 1;
+    console.log("i am btn clicked",counter);
+    setCounter(counter);
+  }
+  let disLikeMeAction=()=>{
+    counter =counter + 1;
+    console.log("i am btn clicked",counter);
+    setCounter(counter);
+  }
+
+  return <>
+  <h1>{counter}</h1>
+  <input type="button" value="Click Me" onClick={likeMeAction} />
+  <input type="button" value="Click for dislike Me" onClick={disLikeMeAction} />
+  </>
 }
 
 
-
-
 export default App;
+
