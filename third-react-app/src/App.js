ec96bf28bb62e4eed96522a44f2fefabb8bcbc3a
Raviraj patil <ravirajpatil298@gmail.com>
2023-06-14 19:33:14 +0530

    basic counter

--- third-react-app/src/App.js
+++ third-react-app/src/App.js
@@ -1,32 +1,30 @@
-import { useState } from "react";
+import {useState} from "react";
+
 function App(){
-  return( <>
-   <h1>Counter App</h1>
-   <CounterApp/>
-  </>
-  );
+  return(
+    <>
+    <h1>Counter App</h1>
+    <CounterApp/>
+    </>
+  )
 }
 
 function CounterApp(){
-  let[counter,setCounter] = useState(1);
-  let likeMeAction=()=>{
-    counter =counter + 1;
-    console.log("i am btn clicked",counter);
-    setCounter(counter);
-  }
-  let disLikeMeAction=()=>{
-    counter =counter + 1;
-    console.log("i am btn clicked",counter);
-    setCounter(counter);
-  }
+  // let counter=1; // stateless
+  //  let list=useState(1); //stateful
+  //  let [counter,setCounter]=list;
+   let [counter,setCounter] =useState(1);
 
-  return <>
-  <h1>{counter}</h1>
-  <input type="button" value="Click Me" onClick={likeMeAction} />
-  <input type="button" value="Click for dislike Me" onClick={disLikeMeAction} />
-  </>
+   let likeMeAction = () =>{
+      counter = counter+1;
+      console.log(counter);
+      setCounter(counter);
+   }
+  return(
+    <>
+    <h1>{counter}</h1>
+    <input type="button" value="Like Me" onClick={likeMeAction} />
+    </>
+  )
 }
-
-
 export default App;
-
